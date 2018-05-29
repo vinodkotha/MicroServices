26301b59ca9ff6591843857ff0c4dc0ef80058c2
vinodkotha <vennivinod@gmail.com>
2018-05-28 22:31:59 -0500

    added

--- limits-service/src/main/java/com/vk/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/vk/limitsservice/LimitsServiceApplication.java
@@ -1,12 +1,35 @@
 package com.vk.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vk.limitsservice.bean.Configuration;
+
 
 @SpringBootApplication
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+	
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+		
+		@Autowired
+		private Configuration config;
+	    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+	    private String msg;
+	 
+	    @RequestMapping("/msg")
+	    int getMsg() {
+	        return config.getMax();
+	    }
 	}
 }
